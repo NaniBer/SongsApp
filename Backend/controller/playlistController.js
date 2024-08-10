8e0d0ee0d6b0fb5c4bc4312d7ff9a7b89ed39fc7
NaniBer <gdgr2010@gmail.com>
2024-08-10 13:06:14 +0300

    Playlist controller added

---
+++ Backend/controller/playlistController.js
@@ -0,0 +1,184 @@
+const Playlist = require("../model/playlist");
+
+const createPlaylist = async (name, description, songs, user) => {
+  try {
+    // Check if the song already exists
+    const existingPlaylist = await Playlist.findOne({ name, user });
+
+    if (existingPlaylist) {
+      // Return an object indicating the song already exists
+      return {
+        success: false,
+        statusCode: 409,
+        message: "Playlist already exists",
+      };
+    }
+
+    // Create a new song
+    const newPlaylist = new Playlist({
+      name,
+      description,
+      songs,
+      user,
+    });
+
+    // Save the new playlist to the database
+    await newPlaylist.save();
+
+    // Return an object indicating success
+    return {
+      success: true,
+      statusCode: 201,
+      message: "Plsylist created successfully",
+      playlist: newPlaylist,
+    };
+  } catch (error) {
+    console.error("Error creating playlist:", error);
+    return {
+      success: false,
+      statusCode: 500,
+      message: "Internal server error",
+    };
+  }
+};
+
+const getPlaylists = async (userId) => {
+  try {
+    // Find all playlists associated with the user
+    const playlists = await Playlist.find({ user: userId });
+
+    return {
+      success: true,
+      playlists,
+    };
+  } catch (error) {
+    console.error("Error retrieving playlists:", error);
+    return {
+      success: false,
+      message: "Failed to retrieve playlists",
+      error: error.message,
+    };
+  }
+};
+
+const updatePlaylist = async (playlistId, name, description, userId) => {
+  try {
+    const existingPlaylist = await Playlist.findById(playlistId);
+
+    if (!existingPlaylist) {
+      return { success: false, statusCode: 404, message: "Playlist not found" };
+    }
+
+    if (existingPlaylist.user.toString() === userId) {
+      existingPlaylist.name = name;
+      existingPlaylist.description = description;
+    } else {
+      return {
+        success: false,
+        statusCode: 403,
+        message: "You have no permission to edit this playlist",
+      };
+    }
+
+    await existingPlaylist.save();
+    return {
+      success: true,
+      statusCode: 200,
+      message: "Playlist updated successfully",
+      playlist: existingPlaylist,
+    };
+  } catch (error) {
+    console.error("Error updating Playlist:", error);
+    throw new Error("Internal server error");
+  }
+};
+
+//delete playlist
+const deletePlaylist = async (playlistId, userId) => {
+  try {
+    const result = await Playlist.deleteOne({ _id: playlistId, user: userId });
+
+    if (result.deletedCount === 0) {
+      return {
+        success: false,
+        statusCode: 404,
+        message: "No playlist found for the given ID and user",
+        deletedCount: 0,
+      };
+    }
+
+    return {
+      success: true,
+      statusCode: 200,
+      message: "Playlist deleted successfully",
+      deletedCount: result.deletedCount,
+    };
+  } catch (error) {
+    console.error("Error deleting playlist:", error);
+    return {
+      success: false,
+      statusCode: 500,
+      message: "Internal server error",
+    };
+  }
+};
+
+//count songs
+const countPlaylists = async (userId) => {
+  try {
+    // Count the number of playlists associated with the user
+    const totalPlaylists = await Playlist.countDocuments({ user: userId });
+
+    return {
+      success: true,
+      totalPlaylists,
+    };
+  } catch (error) {
+    console.error("Error counting playlists:", error);
+    return {
+      success: false,
+      message: "Failed to count playlists",
+      error: error.message,
+    };
+  }
+};
+
+const searchPlaylist = async (name, userId) => {
+  try {
+    // Find playlists by name
+    const playlists = await Playlist.find({ name: name, user: userId });
+
+    // Check if any playlists are found and belong to the user
+    if (playlists.length > 0) {
+      return {
+        success: true,
+        statusCode: 200,
+        data: playlists,
+        message: "Playlist(s) found",
+      };
+    } else {
+      return {
+        success: false,
+        statusCode: 404,
+        message: "No playlist found",
+      };
+    }
+  } catch (error) {
+    console.error("Error searching playlist:", error);
+    return {
+      success: false,
+      statusCode: 500,
+      message: "Failed to search playlist",
+      error: error.message,
+    };
+  }
+};
+
+module.exports = {
+  createPlaylist,
+  updatePlaylist,
+  deletePlaylist,
+  getPlaylists,
+  countPlaylists,
+  searchPlaylist,
+};
--- Backend/routes/playlistRoute.js
+++ Backend/routes/playlistRoute.js
@@ -1,4 +1,179 @@
 const express = require("express");
 const router = express.Router();
+const {
+  createPlaylist,
+  getPlaylists,
+  updatePlaylist,
+  deletePlaylist,
+  countPlaylists,
+  searchPlaylist,
+} = require("../controller/playlistController");
+router.post("/createPlaylist", async (req, res) => {
+  const { name, description, songs, user } = req.body;
+
+  try {
+    const result = await createPlaylist(name, description, songs, user);
+
+    // Check for successful playlist creation
+    if (!result.success) {
+      return res.status(result.statusCode).json({
+        success: result.success,
+        statusCode: result.statusCode,
+        message: result.message,
+      });
+    }
+
+    // Send the successful response with the created playlist
+    res.status(201).json({
+      success: true,
+      statusCode: 201,
+      message: "Playlist created successfully",
+      playlist: result.playlist,
+    });
+  } catch (error) {
+    console.error(
+      "Internal server error occurred while creating the playlist:",
+      error
+    );
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: "Internal server error occurred while creating the playlist.",
+    });
+  }
+});
+
+router.get("/getPlaylists/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await getPlaylists(id);
+
+    if (!result) {
+      return res.status(404).json({
+        success: false,
+        statusCode: 404,
+        message: "No playlist found with the given ID",
+        playlist: null,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      statusCode: 200,
+      message: "Playlist fetched successfully",
+      playlist: result,
+    });
+  } catch (error) {
+    // Handle any errors that occur
+    console.error("Error fetching playlist:", error);
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: "Internal server error",
+      playlist: null,
+    });
+  }
+});
+
+router.put("/updatePlaylist/:id", async (req, res) => {
+  try {
+    const playlistId = req.params.id;
+    const userId = req.body.userid;
+    if (!playlistId) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Invalid Playlist ID" });
+    }
+
+    const { name, description, songs } = req.body;
+    const result = await updatePlaylist(
+      playlistId,
+      name,
+      description,
+      songs,
+      userId
+    );
+
+    res.status(result.statusCode).json({
+      success: result.success,
+      statusCode: result.statusCode,
+      message: result.message,
+      updatedPlaylist: result.playlist,
+    });
+  } catch (error) {
+    console.error("Error updating playlist:", error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+router.delete("/deletePlaylist/:id", async (req, res) => {
+  const songId = req.params.id;
+  const userId = req.body.userId;
+
+  try {
+    const result = await deletePlaylist(songId, userId);
+
+    res.status(result.statusCode).json({
+      success: result.success,
+      statusCode: result.statusCode,
+      message: result.message,
+      playlist: result.playlist,
+    });
+  } catch (error) {
+    console.error("Error deleting playlist:", error);
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: "Internal server error occurred while deleting the playlist.",
+    });
+  }
+});
+router.get("/countPlaylist/:id", async (req, res) => {
+  const userid = req.params.id;
+  try {
+    const result = await countPlaylist(userid);
+
+    // Send the result back in the response
+    res.status(200).json({
+      success: true,
+      statusCode: 200,
+      data: result, // Return the counting result
+      message: "Playlist counted successfully",
+    });
+  } catch (error) {
+    console.error("Error counting playlists:", error);
+
+    // Handle any errors that occur
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: "Internal server error occurred while counting the playlist.",
+    });
+  }
+});
+router.get("/searchPlaylsit", async (req, res) => {
+  const { userid, name } = req.query; // Use req.query for query parameters
+
+  try {
+    const result = await searchPlaylist(userid, name);
+
+    // Send the result back to the client
+    res.status(result.statusCode).json({
+      success: result.success,
+      statusCode: result.statusCode,
+      message: result.message,
+      data: result.data,
+    });
+  } catch (error) {
+    console.error("Error processing searching playlists:", error);
+
+    // Handle any unexpected errors
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: "Internal server error occurred while serching the playlist.",
+    });
+  }
+});
 
 module.exports = router;
